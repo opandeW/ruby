60a960ee80d0ceb1dfdfe6d12155b1341773eba2
opandeW <wopande@gmail.com>
2016-10-08 16:38:09 +0300

    sum and product of two numbers

---
+++ head_first_ruby1/14_chapter14/pool_puzzle/app.rb
@@ -0,0 +1,15 @@
+require 'addition'
+
+get ('/addition') do
+	@first = 3
+	@second = 5
+	@result = @first + @second
+	erb :addition
+end
+
+get ('/multiplication') do
+	@first = 2
+	@second = 6
+	@result = @first * @second
+	erb :multiplication
+end
