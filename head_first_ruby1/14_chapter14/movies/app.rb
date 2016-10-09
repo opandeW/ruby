2fb46c6c6d0a91d2fa29728b52cf1e1f872182d4
opandeW <wopande@gmail.com>
2016-10-09 10:39:11 +0300

    HTML form that sends request

--- head_first_ruby1/14_chapter14/movies/app.rb
+++ head_first_ruby1/14_chapter14/movies/app.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require 'movie' #loading the movie.rb file
 
 get ('/movies') do
-	@movie = Movie.new #Setting up a new movie object
-	@movie.title = "Jaws"
+	@movies = []  #Setting up an array movies
+	@movies[0] = Movie.new
+	@movies[0].title = "Jaws"
+	@movies[1] = Movie.new
+	@movies[1].title = "Alien vs Predator"
+	@movies[2] = Movie.new
+	@movies[2].title = "Terminator 2"
 	erb :index
+end
+
+get ('/movies/new') do #Additon of a second route with a different path
+	erb :new
 end
